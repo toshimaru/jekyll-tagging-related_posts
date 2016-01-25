b4833fc4035842beb7b752bc9539fab6937c95c4
toshi <me@toshimaru.net>
2016-01-25 23:47:17 +0900

    Add Ruby versions for TravisCI

--- lib/jekyll-tagging-related_posts.rb
+++ lib/jekyll-tagging-related_posts.rb
@@ -1,7 +1,8 @@
-require "jekyll/document"
-
 require "jekyll/tagging/related_posts/version"
 require "jekyll/tagging/related_posts"
+
+# require `Jekyll::Document` first to overide 
+require "jekyll/document" 
 
 module Jekyll
   class Document

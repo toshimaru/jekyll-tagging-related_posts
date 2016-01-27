8dde54c760e534f405ebd48bb23230d38996f5ae
toshi <me@toshimaru.net>
2016-01-27 23:17:46 +0900

    Add tests

--- test/jekyll-tagging-related_posts_test.rb
+++ test/jekyll-tagging-related_posts_test.rb
@@ -7,18 +7,23 @@ class TestPage < JekyllUnitTest
     @document = @site.posts.docs.first
   end
 
-  def test_document
-    p @document.related_posts
+  def test_tag_name
+    assert_equal 'foo', @document.data["tags"].first
   end
 
-  def test_related_post
-    @site.reset
-    @site.read
+  def test_related_posts
+    assert_instance_of Array, @document.related_posts
+  end
 
-    last_post     = @site.posts.docs.last
-    related_posts = Jekyll::RelatedPosts.new(last_post).build
+  def test_related_posts
+    assert_instance_of Jekyll::Document, @document.related_posts.first
+  end
 
-    last_10_recent_posts = (@site.posts.docs.reverse - [last_post]).first(10)
-    assert_equal last_10_recent_posts, related_posts
+  def test_related_posts_count
+    assert_equal 2, @document.related_posts.count
+  end
+
+  def test_related_post_tag
+    assert_includes @document.related_posts.first.data["tags"], 'foo'
   end
 end
